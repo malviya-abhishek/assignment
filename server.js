bfcf372f51d446a38073e3c7bdb8fc4b98d454e1
abhishek.malviya <abhishek.malviya@olacabs.com>
2022-09-18 15:14:47 +0530

    front end added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ import bodyParser  from "body-parser";
 import { routesConfig as videoRoutesConfig } from "./routes/routes.api.video.js";
 import { periodicYoutubeApiCaller } from "./app/service/periodicYoutubeApiCaller.service.js";
 
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const app = express();
 
 app.use(cors());
@@ -23,6 +27,14 @@ app.use( (req, res, next)=>{
     next();
 });
 
+
+// serve static file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+})
 
 app.get("/health-check", (req, res)=>{
     res.status(200).json({
